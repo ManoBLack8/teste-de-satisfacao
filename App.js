03c69ddd33452f4436281c1a97c09cd7cccea153
Marcos Tokashiki <marcostokashiki@gmail.com>
2022-07-15 00:02:35 -0400

    Adição do input de senha e do botão confirmar

--- App.js
+++ App.js
@@ -1,24 +1,52 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.textprincipal}>ManoBlack</Text>
       <StatusBar style="auto" />
+      <Text style={styles.textprincipal}>Digite a senha</Text>
+      <TextInput
+      style={styles.input}
+      secureTextEntry={true}
+      />
+      <TouchableOpacity
+        style={styles.button}
+      >
+        <Text>Confirmar</Text>
+      </TouchableOpacity>
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   textprincipal: {
-    color: '#FFFF00',
+    color: '#000',
     fontSize: 30,
+  },
+  input: {
+  borderWidth: 2,
+  borderColor: '#000',
+  borderRadius:10,
+  width: 200,
+  height: 50,
+  margin:10,
+  },
+  button:{
+    borderWidth: 1,
+    borderColor:'#000',
+    borderRadius:5,
+    width:100,
+    height:35,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
