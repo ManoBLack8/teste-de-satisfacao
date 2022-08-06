3ada0c9bfa878878640ecb1155a331cb0d926f61
Marcos Tokashiki <marcostokashiki@gmail.com>
2022-08-06 13:13:44 -0400

    update confirm

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-import React from "react";
 import { StatusBar } from 'expo-status-bar';
-<<<<<<< Updated upstream
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-=======
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
->>>>>>> Stashed changes
 
 export default function App() {
-  
-  
   return (
     <View style={styles.container}>
-<<<<<<< Updated upstream
-=======
       <Image 
       style={styles.logo}
       source={require('@images/logoOt.jpg')}></Image>
@@ -21,21 +12,11 @@ export default function App() {
       <TouchableOpacity style={styles.button}>
       <Text style={styles.buttonText}>OK</Text>
       </TouchableOpacity>
->>>>>>> Stashed changes
       <StatusBar style="auto" />
-      <Text style={styles.textprincipal}>Digite a senha</Text>
-      <TextInput
-      style={styles.input}
-      secureTextEntry={true}
-      />
-      <TouchableOpacity
-        style={styles.button}
-      >
-        <Text>Confirmar</Text>
-      </TouchableOpacity>
     </View>
   );
-};
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -43,29 +24,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
   },
-<<<<<<< Updated upstream
-  textprincipal: {
-    color: '#000',
-    fontSize: 30,
-  },
-  input: {
-  borderWidth: 2,
-  borderColor: '#000',
-  borderRadius:10,
-  width: 200,
-  height: 50,
-  margin:10,
-  },
-  button:{
-    borderWidth: 1,
-    borderColor:'#000',
-    borderRadius:5,
-    width:100,
-    height:35,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-=======
   text:{
     color:"#fff",
     marginVertical:50,
@@ -90,5 +48,4 @@ const styles = StyleSheet.create({
     backgroundColor:"#fff",
     borderRadius:10,
   }
->>>>>>> Stashed changes
 });
