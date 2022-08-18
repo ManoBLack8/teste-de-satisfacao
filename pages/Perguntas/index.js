f18a1aa29393d86cd9e9605d82bd415f48f2bb27
Marcos Tokashiki <marcostokashiki@gmail.com>
2022-08-17 20:55:33 -0400

    update botões de feliz/neutro/triste
    -adicionado ao apertar tirar opacidade até que outro botão seja apertado

--- pages/Perguntas/index.js
+++ pages/Perguntas/index.js
@@ -1,22 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, TouchableHighlight, Pressable } from 'react-native';
 
 
 export default function Home({ navigation }) {
+  const [isPressHappy, setIsPressHappy] = useState(false);
+  const [isPressNeutral, setIsPressNeutral] = useState(false);
+  const [isPressSad, setIsPressSad] = useState(false);
+  const touchPropsHappy = {
+    activeOpacity: 1,
+    underlayColor: 'black',
+    style: isPressHappy ? styles.btnPress : styles.btnNormal,
+    onHideUnderlay: () => setIsPressHappy(true),
+    onShowUnderlay: () => setIsPressHappy(true),
+    onPress: () => {setIsPressSad(false);setIsPressNeutral(false)},
+  };
+  const touchPropsNeutral = {
+    activeOpacity: 1,
+    underlayColor: 'black',
+    style: isPressNeutral ? styles.btnPress : styles.btnNormal,
+    onHideUnderlay: () => setIsPressNeutral(true),
+    onShowUnderlay: () => setIsPressNeutral(true),
+    onPress: () => {setIsPressSad(false);setIsPressHappy(false)},
+  };
+  const touchPropsSad = {
+    activeOpacity: 1,
+    underlayColor: 'black',
+    style: isPressSad ? styles.btnPress : styles.btnNormal,
+    onHideUnderlay: () => setIsPressSad(true),
+    onShowUnderlay: () => setIsPressSad(true),
+    onPress: () => {setIsPressHappy(false);setIsPressNeutral(false)},
+  };
     return (
 <View style={styles.container}>
       <Text style={styles.text}>lorem ipsum</Text>
       <View style={styles.qButtonContainer}>
-      <TouchableOpacity style={styles.questionButton}>
+      <TouchableHighlight {...touchPropsHappy}>
       <Image style={styles.questionImg} source={require('./happyIcon.png')} ></Image>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.questionButton}>
+      </TouchableHighlight>
+      <TouchableHighlight {...touchPropsNeutral}>
       <Image style={styles.questionImg} source={require('./neutralIcon.png')}></Image>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.questionButton}>
+      </TouchableHighlight>
+      <TouchableHighlight {...touchPropsSad}>
       <Image style={styles.questionImg} source={require('./sadIcon.png')}></Image>
-      </TouchableOpacity>
+      </TouchableHighlight>
       </View>
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate ('Obrigado')}>
       <Text style={styles.buttonText}>Proximo</Text>
@@ -45,14 +72,21 @@ export default function Home({ navigation }) {
     qButtonContainer:{
         flexDirection:'row',
         marginVertical:50,
-    },
-    questionButton:{
-        backgroundColor:'#302D2D',
-        width:125,
-        height:125,
-        marginHorizontal:60,
-        borderRadius:15,
         
+    },
+    btnPress: {
+      opacity:0.2,
+      backgroundColor:'white',
+      borderRadius: 25,
+      width:125,
+      height:125,
+      marginHorizontal:50,
+    },
+    btnNormal: {
+      borderRadius: 25,
+      width:125,
+      height:125,
+      marginHorizontal:50,
     },
     button:{
       backgroundColor: "#F4EFF4",
