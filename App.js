b71c3b619ce1eae7df918f0570afe14478f72981
Marcos Tokashiki <marcostokashiki@gmail.com>
2022-08-27 02:14:55 -0400

    deletando page adm e deletando o header das paginas

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import * as ScreenOrientation from 'expo-screen-orientation'
 import Home from './pages/Home';
-import Adm from './pages/Adm';
 import Perguntas from './pages/Perguntas';
 import Obrigado from './pages/Obrigado';
 import Dados from './pages/Dados';
@@ -28,8 +27,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Adm" component={ Adm } />
+      <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={ Home } />
         <Stack.Screen name="Perguntas" component={ Perguntas } />
         <Stack.Screen name="Obrigado" component={ Obrigado } />
